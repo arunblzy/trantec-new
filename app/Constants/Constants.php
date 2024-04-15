3e530776a74d6e29fd7ab3f35d6e14e1dfa24a11
arun_raj_kv <arunblzy06@gmail.com>
2024-04-15 14:14:21 +0530

    url changed

--- app/Constants/Constants.php
+++ app/Constants/Constants.php
@@ -2,12 +2,9 @@
 namespace App\Constants;
 
 class Constants {
-    const SERVICE_BASE_URL = '/api/services';
+    const SERVICE_BASE_URL = '/api/suppliers';
     const SUPPLIERS_QUERY_STRING = 'suppliers';
     const APPEND_SUPPLIERS_QUERY_STRING = '?tag=suppliers';
-    const APPEND_INVOICES_QUERY_STRING = '?tag=invoices';
     const ALL_SUPPLIERS_URL = '/api/suppliers';
-    const STORE_SUPPLIER_URL = '/api/services?tag=suppliers';
-    const ALL_INVOICES_URL = '/api/services?tag=invoices';
-    const STORE_INVOICE_URL = '/api/services?tag=invoices';
+    const STORE_SUPPLIER_URL = '/api/suppliers';
 }
