86b97df57d5dec178606810a65c54c7cfb491af7
arun_raj_kv <arunblzy06@gmail.com>
2024-04-18 19:49:32 +0530

    minor fixes

--- resources/views/pages/suppliers/edit.blade.php
+++ resources/views/pages/suppliers/edit.blade.php
@@ -148,7 +148,7 @@
                                             </div>
                                             <div class="col-md-2">
                                                 <label class="form-label">Fax:</label>
-                                                <input type="text" name="contact_fax[]"
+                                                <input type="text" name="contact_fax[]" value="{{ $item->fax }}"
                                                     class="form-control form-control-lg
                                                 form-control-solid">
                                             </div>
